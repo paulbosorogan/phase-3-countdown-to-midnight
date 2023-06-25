a2b9639e58b50fba96ae659f1805f601cfecdc27
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-06-25 10:08:20 -0400

    commit tree f9d1303af4ef2681c4798be19e822a31dab426c1

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown num 
+    x=1 
+    while num >= 1
+        puts "#{num} SECOND(S)!"
+        num -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep num
+    while num > 0 
+        puts "#{num} SECOND(S)!"
+        sleep(1)
+        num -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
